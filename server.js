c66796cbbc7db9b13cac71f2903d7c2621c4dfe3
K Lasya Rao <23951a055u@iare.ac.in>
2025-07-11 11:49:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());                      // 🟢 This must come before your routes
 app.use(express.static('public'));            // Serve your frontend from /public
